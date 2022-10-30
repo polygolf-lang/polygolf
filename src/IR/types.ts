a05556b317ca5035d8ec71047f8cec83a27932a1
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-10-30 22:36:02 +0100

    add type operations

--- src/IR/types.ts
+++ src/IR/types.ts
@@ -87,3 +87,106 @@ function bigIntMinAndMax(args: bigint[]) {
 export function annotate(expr: Expr, valueType: ValueType): Expr {
   return { ...expr, valueType };
 }
+
+export function toString(a: ValueType): string {
+  switch (a.type) {
+    case "List":
+      return `(List ${toString(a.member)})`;
+    case "Array":
+      return `(Array ${toString(a.member)} ${a.length})`;
+    case "Set":
+      return `(Set ${toString(a.member)})`;
+    case "Table":
+      return `(Table ${toString(a.key)} ${toString(a.value)})`;
+    case "void":
+      return "Void";
+    case "string":
+      return "Text";
+    case "boolean":
+      return "Bool";
+    case "integer":
+      return `${a.low === undefined ? "-oo" : a.low.toString()}..${
+        a.low === undefined ? "oo" : a.low.toString()
+      }`;
+  }
+  throw new Error("Unknown type.");
+}
+
+export function union(a: ValueType, b: ValueType): ValueType {
+  if (a.type !== b.type) {
+    throw new Error(`Cannot model union of ${toString(a)} and ${toString(b)}.`);
+  }
+  try {
+    switch (a.type) {
+      case "List":
+        return listType(union(a.member, (b as any).member as ValueType));
+      case "Array":
+        if (a.length !== (b as any).length)
+          throw new Error(
+            `Cannot model union of ${toString(a)} and ${toString(b)}.`
+          );
+        return arrayType(
+          union(a.member, (b as any).member as ValueType),
+          a.length
+        );
+      case "Set":
+        return setType(union(a.member, (b as any).member as ValueType));
+      case "Table":
+        return tableType(
+          union(a.key, (b as any).key as ValueType) as any,
+          union(a.value, (b as any).value as ValueType)
+        );
+      case "integer":
+        return b.type === "integer"
+          ? integerType(
+              a.low === undefined || b.low === undefined
+                ? undefined
+                : a.low < b.low
+                ? a.low
+                : b.low,
+              a.high === undefined || b.high === undefined
+                ? undefined
+                : a.high < b.high
+                ? b.high
+                : a.high
+            )
+          : integerType();
+      default:
+        return a;
+    }
+  } catch (e) {
+    throw new Error(
+      `Cannot model union of ${toString(a)} and ${toString(b)}.\n${
+        e instanceof Error ? e.message : ""
+      }`
+    );
+  }
+}
+
+/** Determines if `a` is a subtype of `b`. */
+export function isSubtype(a: ValueType, b: ValueType): boolean {
+  if (a.type !== b.type) {
+    return false;
+  }
+  switch (a.type) {
+    case "Set":
+    case "List":
+      return isSubtype(a.member, (b as any).member);
+    case "Array":
+      return (
+        a.length === (b as any).length && isSubtype(a.member, (b as any).member)
+      );
+    case "Table":
+      return (
+        isSubtype(a.key, (b as any).key) && isSubtype(a.value, (b as any).value)
+      );
+    case "integer":
+      return (
+        b.type === "integer" &&
+        (b.low === undefined || (a.low !== undefined && a.low >= b.low)) &&
+        (b.high === undefined || (a.high !== undefined && b.high >= a.high))
+      );
+    default:
+      return true;
+  }
+}
