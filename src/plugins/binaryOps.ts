41bb5a8320eb0c69be71ac2408fc3a19c889b373
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-11-12 15:27:49 +0100

    add flipBinaryOps

--- src/plugins/binaryOps.ts
+++ src/plugins/binaryOps.ts
@@ -1,8 +1,14 @@
-import { mutatingBinaryOp } from "../IR";
-import { Path } from "../common/traverse";
+import {
+  flipOpCode,
+  isBinary,
+  mutatingBinaryOp,
+  polygolfOp,
+  variants,
+} from "../IR";
+import { Path, Visitor } from "../common/traverse";
 
 // "a = a + b" --> "a += b"
-export const addMutatingBinaryOp = {
+export const addMutatingBinaryOp: Visitor = {
   enter(path: Path) {
     const node = path.node;
     if (node.type === "Assignment" && node.expr.type === "BinaryOp") {
@@ -19,23 +25,30 @@ export const addMutatingBinaryOp = {
             JSON.stringify(node.expr.left.index))
       ) {
         path.replaceWith(
-          mutatingBinaryOp(node.expr.op, node.variable, node.expr.right)
+          mutatingBinaryOp(
+            node.expr.op,
+            node.variable,
+            node.expr.right,
+            node.expr.name
+          )
         );
       }
-      // "a = b + a" --> "a += b"
-      /* This requires to keep information about the semantics of a operator
-    else if (node.expr.right.type === "Identifier" 
-    && node.variable.name === node.expr.right.name
-    && is_symmetric(node.expr.op)) {
-      path.replaceWith(
-        mutatingBinaryOp(
-          node.expr.op,
-          node.variable,
-          node.expr.left
-        )
-      );
     }
-    */
+  },
+};
+
+// "a + b; --> {a + b; b + a}"
+export const flipBinaryOps: Visitor = {
+  generatesVariants: true,
+  exit(path: Path) {
+    const node = path.node;
+    if (node.type === "PolygolfOp" && isBinary(node.op)) {
+      const flipped = flipOpCode(node.op);
+      if (flipped !== null) {
+        path.replaceWith(
+          variants([node, polygolfOp(flipped, node.args[1], node.args[0])])
+        );
+      }
     }
   },
 };
