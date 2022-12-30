9027c333d35ba1a4a6a46fa898852b9192386407
H <huntertr2002@gmail.com>
2022-12-29 21:16:57 -0600

    Clean up imports

--- src/languages/swift/plugins.ts
+++ src/languages/swift/plugins.ts
@@ -1,22 +1,21 @@
 import {
-    block,
-    ImportStatement,
-    importStatement,
-    Program,
-  } from "../../IR";
-  import { Path, Visitor } from "../../common/traverse";
-  import { getType } from "../../common/getType";
+  block,
+  ImportStatement,
+  importStatement,
+  Program,
+} from "../../IR";
+import { Visitor } from "../../common/traverse";
 
 export const addImports: Visitor = {
-    name: "addImports",
-    enterProgram(program: Program) {
-      const dependencies = [...program.dependencies];
-      if (dependencies.length < 1) return;
-      let imports: ImportStatement;
-      imports ??= importStatement("import", dependencies);
-      program.body =
-        program.body.kind === "Block"
-          ? block([imports, ...program.body.children])
-          : block([imports, program.body]);
-    },
-  };
+  name: "addImports",
+  enterProgram(program: Program) {
+    const dependencies = [...program.dependencies];
+    if (dependencies.length < 1) return;
+    let imports: ImportStatement;
+    imports ??= importStatement("import", dependencies);
+    program.body =
+      program.body.kind === "Block"
+        ? block([imports, ...program.body.children])
+        : block([imports, program.body]);
+  },
+};
