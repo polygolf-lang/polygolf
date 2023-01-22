5fae30aa684b71202bd3a3fafb4a7f59733d1112
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2023-01-22 12:56:25 +0100

    Hashing (#70)
    * prepare tableHashing plugin
    
    * finish tableHashing plugin
    
    * use tableHashing plugin in Nim
    
    * remove console.log
    
    * reuse result array, use null instead of undefined
    in findHash
    
    * add hashing test with hole

---
+++ src/languages/nim/hash.test.ts
@@ -0,0 +1,18 @@
+import hash from "./hash";
+
+function testString(x: string, h: number) {
+  test(x, () => {
+    expect(hash(x)).toEqual(h);
+  });
+}
+
+describe("Nim hash", () => {
+  testString("", 0);
+  testString("code.golf", 3712193236);
+  testString("▄ ▄▄▄", 1795570265);
+  testString("💎✂", 2632446085);
+  testString(
+    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    1622063614
+  );
+});
---
+++ src/plugins/hashing.ts
@@ -0,0 +1,124 @@
+import { getType } from "../common/getType";
+import { Plugin } from "../common/Language";
+import {
+  defaultValue,
+  Expr,
+  functionCall,
+  int,
+  listConstructor,
+  polygolfOp,
+  StringLiteral,
+} from "../IR";
+
+/**
+ *
+ * @param hashFunc Behaviour of the builtin hash function used.
+ * @param hashNode Recipe for invoking the bultin hash function.
+ * @param maxMod Max value of `mod`.
+ * @returns Replaces `tableLiteral[key]` with `listLiteral[hash(key)%mod%len(listLiteral)]`,
+ * where `hash` is the builtin used, `mod` <= `maxMod` and `listLiteral` contains values from `tableLiteral` and holes.
+ */
+export function tableHashing(
+  hashFunc: (x: string) => number,
+  hashNode: string | ((x: Expr) => Expr) = "hash",
+  maxMod = 99999
+): Plugin {
+  let hash: (x: Expr) => Expr;
+  if (typeof hashNode === "string") {
+    hash = (x: Expr) => functionCall([x], hashNode);
+  } else {
+    hash = hashNode;
+  }
+  return {
+    name: "tableHashing(...)",
+    visit(node, spine) {
+      if (
+        node.kind === "PolygolfOp" &&
+        node.op === "table_get" &&
+        node.args[0].kind === "TableConstructor"
+      ) {
+        const table = node.args[0];
+        const getKey = node.args[1];
+        const tableType = getType(table, spine.root.node);
+        if (
+          tableType.kind === "Table" &&
+          tableType.key.kind === "text" &&
+          table.kvPairs.every((x) => x.key.kind === "StringLiteral")
+        ) {
+          const [array, mod] = findHash(
+            hashFunc,
+            table.kvPairs.map((x) => [(x.key as StringLiteral).value, x.value]),
+            maxMod
+          );
+          let lastUsed = array.length - 1;
+          while (array[lastUsed] === null) lastUsed--;
+
+          return polygolfOp(
+            "list_get",
+            listConstructor(
+              array
+                .slice(0, lastUsed + 1)
+                .map((x) => x ?? defaultValue(tableType.value))
+            ),
+            polygolfOp(
+              "mod",
+              mod === array.length
+                ? hash(getKey)
+                : polygolfOp("mod", hash(getKey), int(mod)),
+              int(array.length)
+            )
+          );
+        }
+      }
+    },
+  };
+}
+
+function findHash( // TODO: Allow collisions in keys that map to the same value.
+  hashFunc: (x: string) => number,
+  table: [string, Expr][],
+  maxMod: number
+): [(Expr | null)[], number] {
+  let width = table.length;
+  const hashedTable: [number, Expr][] = table.map((x) => [
+    hashFunc(x[0]),
+    x[1],
+  ]);
+  const result: (Expr | null)[] = Array(width);
+  while (true) {
+    for (let mod = width; mod <= maxMod; mod++) {
+      result.fill(null);
+      let collision = false;
+      for (const [key, value] of hashedTable) {
+        const i = (key % mod) % width;
+        if (result[i] !== null) {
+          collision = true;
+          break;
+        }
+        result[i] = value;
+      }
+      if (!collision) {
+        return [result, mod];
+      }
+    }
+    width++;
+    result.push(null);
+  }
+}
+
+// a simple hashFunc to test the plugin
+function javaHash(str: string): number {
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+    const chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0;
+  }
+  return (hash + 2 ** 32) % 2 ** 32;
+}
+export function testTableHashing(maxMod: number): Plugin {
+  return {
+    ...tableHashing(javaHash, "hash", maxMod),
+    name: `testTableHashing(${maxMod})`,
+  };
+}
