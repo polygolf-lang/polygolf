2b3407d0eaa2b8c1b54c6c0cf8bb4866836adeb0
H <huntertr2002@gmail.com>
2023-02-12 23:47:39 -0600

    lint

--- src/languages/swift/index.ts
+++ src/languages/swift/index.ts
@@ -156,7 +156,7 @@ const swiftLanguage: Language = {
       nextToken: string
     ): boolean {
       return (
-        (/^[-+*\/<>=^*|~]+$/.test(token) && /[-~]/.test(nextToken[0])) ||
+        (/^[-+*/<>=^*|~]+$/.test(token) && /[-~]/.test(nextToken[0])) ||
         token === `!=`
       );
     }
