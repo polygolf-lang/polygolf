83294f58da07c70ea1675ea8d0852c78b73ca0fd
H <huntertr2002@gmail.com>
2023-05-03 18:04:56 -0500

    run prettier

--- src/languages/swift/index.ts
+++ src/languages/swift/index.ts
@@ -23,7 +23,11 @@ import { renameIdents } from "../../plugins/idents";
 import { golfStringListLiteral } from "../../plugins/static";
 import { golfLastPrint, implicitlyConvertPrintArg } from "../../plugins/print";
 import { assertInt64 } from "../../plugins/types";
-import { addVarDeclarations, groupVarDeclarations, noStandaloneVarDeclarations } from "../../plugins/block";
+import {
+  addVarDeclarations,
+  groupVarDeclarations,
+  noStandaloneVarDeclarations,
+} from "../../plugins/block";
 import {
   forArgvToForEach,
   forRangeToForRangeInclusive,
