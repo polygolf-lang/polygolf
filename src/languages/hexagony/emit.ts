5d821ab83f98cdaa5766838623cf0eedc173a65c
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2023-05-22 22:57:50 +0200

    prepare emit

--- src/languages/hexagony/emit.ts
+++ src/languages/hexagony/emit.ts
@@ -1,6 +1,47 @@
+import { PolygolfError } from "@/common/errors";
 import { TokenTree } from "@/common/Language";
-import { IR } from "../../IR";
+import { Expr, Program } from "../../IR";
+import { Pointer, ProgramEdgeData, resolveEdges } from "./memory";
 
-export default function emitProgram(program: IR.Program): TokenTree {
-  return "TODO";
+export default function emitProgram(program: Program): TokenTree {
+  const edgeData = resolveEdges(program);
+  const pointer = new Pointer({ x: 0, y: 0, i: 0 }, true);
+  return emitExpr(program.body, edgeData, pointer);
+}
+
+function emitExpr(
+  expr: Expr,
+  edgeData: ProgramEdgeData,
+  pointer: Pointer
+): TokenTree {
+  const edges = edgeData.get(expr);
+  if (edges === undefined)
+    throw new PolygolfError(`Cannot emit expr.`, expr.source);
+  switch (expr.kind) {
+    case "FunctionCall":
+      return [pointer.goTo(edges.appliedTo), expr.ident.name];
+    case "Assignment":
+      switch (expr.expr.kind) {
+        case "IntegerLiteral":
+          return [
+            pointer.goTo(edges.appliedTo),
+            String.fromCodePoint(Number(expr.expr.value)),
+          ];
+        case "Identifier":
+          return [pointer.goAlongAndCopy(edges.postCopyPath ?? [])];
+        case "BinaryOp":
+          if (
+            expr.expr.left.kind === "Identifier" &&
+            expr.expr.right.kind === "Identifier"
+          ) {
+            return [
+              (edges.preCopyPaths ?? []).map((x) => pointer.goAlongAndCopy(x)),
+              pointer.goTo(edges.appliedTo, edges.isCw),
+              expr.expr.name,
+              pointer.goAlongAndCopy(edges.postCopyPath ?? []),
+            ];
+          }
+      }
+  }
+  throw new PolygolfError(`Cannot emit node ${expr.kind}.`, expr.source);
 }
--- src/languages/hexagony/memory.ts
+++ src/languages/hexagony/memory.ts
@@ -1,4 +1,6 @@
-// type EdgeSymbol = 0 | 1 | 2;
+import { Program } from "@/IR";
+
+type EdgeSymbol = 0 | 1 | 2;
 
 /**
  * Edges are indexed by the axial coordinates of the (westward)
@@ -6,10 +8,54 @@
  * which of the three edges is meant.
  */
 
-/*
 interface Edge {
   x: number;
   y: number;
   i: EdgeSymbol;
 }
-*/
+
+export class Pointer {
+  edge: Edge;
+  isCw: boolean;
+
+  constructor(edge: Edge, isCw: boolean) {
+    this.edge = edge;
+    this.isCw = isCw;
+  }
+
+  goTo(edge: Edge, isCw?: boolean): string {
+    return "TODO";
+  }
+  goAlongAndCopy(edges: Edge[]): string {
+    return "TODO";
+  }
+}
+
+type Expr = { kind: string };
+type CopyPath = Edge[];
+type NodeEdgeData = {
+  preCopyPaths?: CopyPath[];
+  postCopyPath?: CopyPath;
+  appliedTo: Edge;
+  negateResult?: boolean;
+  isCw?: boolean | undefined;
+};
+
+export type ProgramEdgeData = Map<Expr, NodeEdgeData>;
+
+/**
+ * Bind based on binary interactions first.
+ *
+ * Strategies according to the number of variables already bound:
+ * 0		Simply bind all to 3 neighbouring registers, preferring recyclation.
+ * 1		Try binding the other 2 to the 2 neigbouring registers (try both sides), provided they are available. If the op is + or *, try both orders.
+ * 2		Find shortest path between the two, try binding the third to every corresponding third edge to any two neigbouring edges on that path, provided it is available.
+ * 		  For +,* allow both directions. For - allow it too, but mind the sign of the result.
+ * 3		Unless they are bound perfectly, pretend one is unbound and go as in previous point.
+ *
+ * Then, traverse again and bind variables that are still unbound.
+ */
+export function resolveEdges(program: Program): ProgramEdgeData {
+  const result = new Map<Expr, NodeEdgeData>(); // TODO
+  return result;
+}
